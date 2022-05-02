8aa337f6d7d804e9aa7ca9747b5a5922ea57c9e3
golden.li <magic.lixin@gmail.com>
2022-05-02 15:52:18 +0800

    update build script

--- make_android.sh
+++ make_android.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-NDK=/Users/mac/Project/ndk/
+NDK=/Users/golden/workspace/ndk/
 API=android-26
 
-./cmake_android.sh build_android -DURHO3D_PHYSICS=1 -DURHO3D_LUA=0 -DURHO3D_URHO2D=0 -DURHO3D_NETWORK=0 -DURHO3D_NAVIGATION=0 \
+./cmake_android.sh build_android -DURHO3D_PHYSICS=1 -DURHO3D_LUA=0 -DURHO3D_URHO2D=1 -DURHO3D_NETWORK=0 -DURHO3D_NAVIGATION=1 \
                                  -DURHO3D_TOOLS=1 -DURHO3D_SAMPLES=0 -DURHO3D_EXTRAS=0 -DURHO3D_FILEWATCHER=0 -DURHO3D_ANGELSCRIPT=1 \
                                  -DURHO3D_WEBP=0 -DURHO3D_IK=1 -DURHO3D_LIB_TYPE=SHARED \
                                  -DANDROID_NDK=$NDK  "$@"
