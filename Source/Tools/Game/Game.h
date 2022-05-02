01426c2307a6555789d1da3c0c3f7645d61528a3
golden.li <magic.lixin@gmail.com>
2022-05-02 16:21:35 +0800

    update game code

--- Source/Tools/Game/Game.h
+++ Source/Tools/Game/Game.h
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2008-2017 the Urho3D project.
+// Copyright (c) 2008-2018 the Urho3D project.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -22,28 +22,82 @@
 
 #pragma once
 
-#include <Urho3D/Engine/Application.h>
+#include "Sample.h"
 
-using namespace Urho3D;
+namespace Urho3D {
 
-class Game : public Application
-{
-    URHO3D_OBJECT(Game, Application);
+class Node;
+class Scene;
+
+} // namespace Urho3D
+
+/// Physics example.
+/// This sample demonstrates:
+///     - Creating both static and moving physics objects to a scene
+///     - Displaying physics debug geometry
+///     - Using the Skybox component for setting up an unmoving sky
+///     - Saving a scene to a file and loading it to restore a previous state
+class Physics : public Sample {
+  URHO3D_OBJECT(Physics, Sample);
 
 public:
-    /// Construct.
-    Game(Context* context);
+  /// Construct.
+  explicit Physics(Context *context);
 
-    /// Setup before engine initialization. Verify that a script file has been specified.
-    virtual void Setup() override;
-    /// Setup after engine initialization. Load the script and execute its start function.
-    virtual void Start() override;
-    /// Cleanup after the main loop. Run the script's stop function if it exists.
-    virtual void Stop() override;
+  /// Setup after engine initialization and before running the main loop.
+  void Start() override;
+
+protected:
+  /// Return XML patch instructions for screen joystick layout for a specific
+  /// sample app, if any.
+  String GetScreenJoystickPatchString() const override {
+    return "<patch>"
+           "    <remove sel=\"/element/element[./attribute[@name='Name' and "
+           "@value='Button0']]/attribute[@name='Is Visible']\" />"
+           "    <replace sel=\"/element/element[./attribute[@name='Name' and "
+           "@value='Button0']]/element[./attribute[@name='Name' and "
+           "@value='Label']]/attribute[@name='Text']/@value\">Spawn</replace>"
+           "    <add sel=\"/element/element[./attribute[@name='Name' and "
+           "@value='Button0']]\">"
+           "        <element type=\"Text\">"
+           "            <attribute name=\"Name\" value=\"MouseButtonBinding\" "
+           "/>"
+           "            <attribute name=\"Text\" value=\"LEFT\" />"
+           "        </element>"
+           "    </add>"
+           "    <remove sel=\"/element/element[./attribute[@name='Name' and "
+           "@value='Button1']]/attribute[@name='Is Visible']\" />"
+           "    <replace sel=\"/element/element[./attribute[@name='Name' and "
+           "@value='Button1']]/element[./attribute[@name='Name' and "
+           "@value='Label']]/attribute[@name='Text']/@value\">Debug</replace>"
+           "    <add sel=\"/element/element[./attribute[@name='Name' and "
+           "@value='Button1']]\">"
+           "        <element type=\"Text\">"
+           "            <attribute name=\"Name\" value=\"KeyBinding\" />"
+           "            <attribute name=\"Text\" value=\"SPACE\" />"
+           "        </element>"
+           "    </add>"
+           "</patch>";
+  }
 
 private:
-    /// Script file name.
-    String scriptFileName_;
-    /// Flag whether CommandLine.txt was already successfully read.
-    bool commandLineRead_;
+  /// Construct the scene content.
+  void CreateScene();
+  /// Construct an instruction text to the UI.
+  void CreateInstructions();
+  /// Set up a viewport for displaying the scene.
+  void SetupViewport();
+  /// Subscribe to application-wide logic update and post-render update events.
+  void SubscribeToEvents();
+  /// Read input and moves the camera.
+  void MoveCamera(float timeStep);
+  /// Spawn a physics object from the camera position.
+  void SpawnObject();
+  /// Handle the logic update event.
+  void HandleUpdate(StringHash eventType, VariantMap &eventData);
+  /// Handle the post-render update event.
+  void HandlePostRenderUpdate(StringHash eventType, VariantMap &eventData);
+
+  /// Flag for drawing debug geometry.
+  bool drawDebug_;
 };
