d7aa47bddd1278e8e760f797b79d0d0275cab3c5
golden.li <magic.lixin@gmail.com>
2022-06-25 18:11:05 +0800

    update asset script

--- asset/batch_convert_fbx.py
+++ asset/batch_convert_fbx.py
@@ -4,8 +4,10 @@ import sys
 import os
 import glob
 import subprocess
+import time
 
-model_convert_script='./convert_model.py'
+model_convert_script='./convert_fbx.py'
+mat_error_file = '/tmp/asset_mat_error'
 
 if __name__ == "__main__":
 
@@ -22,19 +24,32 @@ if __name__ == "__main__":
 
 
             """)
-    print ("batch_convert_model.py [input fbx path] [options]")
+    print ("batch_convert_fbx.py [input fbx path] [options]")
 
     input_path = sys.argv[1] + '/'
 
     search_pat = input_path + '**/*.fbx'
     fbx_files = glob.glob(search_pat, recursive=True)
 
+    args = ' -a /Users/golden/Downloads/game_resource/Life_Is_Strange_1/ '
+
+    t1 = time.time()
+
+    num_of_fbx = len(fbx_files)
+    mat_error_fbx_files = []
+
     #print (fbx_files)
     for fbx in fbx_files:
-        cmd = 'python3 ' + model_convert_script + ' ' + fbx + ' Export_Objects/'
+        cmd = 'python3 ' + model_convert_script + ' ' + fbx + ' Export_Objects/' + args
         # print (cmd)
         os.system(cmd)
+        if os.path.exists(mat_error_file):
+            mat_error_fbx_files.append(fbx)
 
+    t2 = time.time()
+    print ("fbx converted = " + str(num_of_fbx) + " time cost=" + str(int(t2 - t1)) + " secs")
 
+    print ("material error fbx num-files:" + str(len(mat_error_fbx_files)))
+    print (mat_error_fbx_files)
 
 
--- asset/convert_fbx.py
+++ asset/convert_fbx.py
@@ -9,27 +9,70 @@ model_args = "-mb 64 -t -np -na -l"
 assimp_tool = "/build_osx/bin/tool/AssetImporter"
 git_root_cmd = "git rev-parse --show-toplevel"
 output_path = './export/'
-raw_asset_path = '/Users/golden/Downloads/game_resource/Life_Is_Strange_1/'
 mat_template_file = './mat_template.xml'
 obj_template_file = './obj_template.xml'
 character_template_file = './character_template.xml'
 asset_output_folder = 'GameData/'
 # asset_output_path = 'Export_Objects/'
+mat_error_file = '/tmp/asset_mat_error'
 
 def prepare_dir(dir):
     os.system("mkdir -p " + dir)
 
-def process_material(mat_name, output_folder, output_model_name, mat_template, asset_output_path, b_character, b_overwrite):
-    print ("processing material " + mat_name)
+def report_error():
+    os.system('touch ' + mat_error_file)
 
-    search_pat = raw_asset_path + '**/' + mat_name + '*.mat'
+def guess_mat_file(asset_path, mat_name, output_model_name):
+    search_pat = asset_path + '**/' + mat_name + '*.mat'
     mat_files = glob.glob(search_pat, recursive=True)
+
+    # attempt #1
+    if len(mat_files) == 0:
+        #print (mat_name + ' not found !!')
+        # in life strange material will be MT_xxx
+        mat_name = output_model_name.replace("ST_", "MT_")
+        print ("try search with " + mat_name)
+        search_pat = asset_path + '**/' + mat_name + '*.mat'
+        mat_files = glob.glob(search_pat, recursive=True)
+
+    # attempt #2
+    if len(mat_files) == 0:
+        if str(mat_name[-1]).isupper():
+            mat_name = mat_name[:-1]
+        if str(mat_name[-1]) == '_':
+            mat_name = mat_name[:-1]
+        print ("try search with " + mat_name)
+        search_pat = asset_path + '**/' + mat_name + '*.mat'
+        mat_files = glob.glob(search_pat, recursive=True)
+
+    # attempt #3
+    if len(mat_files) == 0:
+        if str(mat_name[-1]).isdigit():
+            mat_name = mat_name[:-1]
+        if str(mat_name[-1]).isdigit():
+            mat_name = mat_name[:-1]
+
+        print ("try search with " + mat_name)
+        search_pat = asset_path + '**/' + mat_name + '*.mat'
+        mat_files = glob.glob(search_pat, recursive=True)
+
 
     if len(mat_files) == 0:
         print (mat_name + ' not found !!')
-        return
+        report_error()
+        return ""
 
-    mat_file = mat_files[0]
+    return mat_files[0]
+
+def process_material(mat_name, output_folder,
+                     output_model_name, mat_template, asset_output_path,
+                     b_character, b_overwrite, asset_path):
+    print ("processing material " + mat_name)
+
+    mat_file = guess_mat_file(asset_path, mat_name, output_model_name)
+
+    if (mat_file == ""):
+        return;
 
     diff = None
     specular = None
@@ -60,9 +103,9 @@ def process_material(mat_name, output_folder, output_model_name, mat_template, a
         if tex_words[0] == 'Emissive':
             emissive = tex_words[1]
 
-        if tex_words[1].endswith('_D'):
+        if tex_words[1].endswith('_D') and not diff:
             diff = tex_words[1]
-        if tex_words[1].endswith('_N'):
+        if tex_words[1].endswith('_N') and not normal:
             normal = tex_words[1]
 
     # if diff is None:
@@ -82,11 +125,18 @@ def process_material(mat_name, output_folder, output_model_name, mat_template, a
     output_texture_folder = output_folder + "Textures/"
     texture_base_path = asset_output_path + output_model_name + "/Textures/"
 
+    diff_tex_found = False
+
     if diff:
         os.system('cp -f ' + diff + ' ' + output_texture_folder)
+        diff_tex_found =  os.path.exists(diff)
         diff = texture_base_path +  os.path.basename(diff)
     else:
         diff = "BaseWhite"
+        diff_tex_found = False
+
+    if not diff_tex_found:
+        report_error()
 
     if normal:
         os.system('cp -f ' + normal + ' ' + output_texture_folder)
@@ -134,7 +184,7 @@ def process_material(mat_name, output_folder, output_model_name, mat_template, a
 
     if os.path.exists(output_mat_file) and not b_overwrite:
         print (output_mat_file + ' already exist!')
-        return
+        return mat_name
 
     with open(output_mat_file, 'w') as output_mat:
         for mat_line in mat_lines:
@@ -142,6 +192,7 @@ def process_material(mat_name, output_folder, output_model_name, mat_template, a
                 continue
             output_mat.write(mat_line + '\n')
 
+    return mat_name
 
 def find_texture(mat_path, text_name):
     if not text_name:
@@ -202,6 +253,7 @@ if __name__ == "__main__":
     print ("convert_model.py [input model file] [output path] [options]")
     print ("-f to force overwrite object xml file")
     print ("-c to specify the character")
+    print ("-a to specify the asset path")
 
     argv = sys.argv
     argv = argv[1:]
@@ -210,16 +262,23 @@ if __name__ == "__main__":
     git_root = subprocess.getoutput(git_root_cmd)
     print (git_root)
 
+    raw_asset_path = '/Users/golden/Downloads/game_resource/Life_Is_Strange_1/'
+    os.system('rm ' + mat_error_file)
+
     input_model = argv[0]
     asset_output_path = argv[1]
     b_overwrite = False
     b_character = False
 
-    for arg in argv:
+    for idx, arg in enumerate(argv):
         if arg == '-f':
             b_overwrite = True
         if arg == '-c':
             b_character = True
+        if arg == '-a':
+            raw_asset_path = argv[idx+1]
+
+    print (raw_asset_path)
 
     file_name_without_ext = os.path.splitext(input_model)[0]
     output_model_name = os.path.basename(file_name_without_ext)
@@ -233,6 +292,10 @@ if __name__ == "__main__":
 
     output_model = output_folder + output_model_name +  ".mdl"
     output_txt = output_folder + output_model_name +  ".txt"
+
+    if not b_overwrite and os.path.exists(output_model):
+        print (output_model + " already exist")
+        quit()
 
     tool = git_root + assimp_tool
 
@@ -256,13 +319,26 @@ if __name__ == "__main__":
         mat_list = f.read().splitlines()
 
     #os.system('rm ' + output_txt)
+    mat_error_happened = False
 
-    for mat_file in mat_list:
+    for idx, mat_file in enumerate(mat_list):
         mat_name = os.path.splitext(mat_file)[0]
         mat_name = os.path.basename(mat_name)
 
-        process_material(mat_name, output_folder, output_model_name, mat_template, asset_output_path, b_character, b_overwrite)
+        new_mat_name = process_material(mat_name, output_folder,
+                                      output_model_name, mat_template,
+                                      asset_output_path, b_character,
+                                      b_overwrite, raw_asset_path)
 
+        mat_list[idx] = 'Materials/' + new_mat_name + '.xml'
 
-    process_object(output_model_name, output_folder, output_model_name, obj_template, mat_list, b_overwrite, asset_output_path)
+        if os.path.exists(mat_error_file):
+            mat_error_happened = True
+            break
+
+    process_object(output_model_name, output_folder, output_model_name,
+                   obj_template, mat_list, b_overwrite, asset_output_path)
+
+    if mat_error_happened:
+        os.system('rm -rf ' + output_folder)
 
